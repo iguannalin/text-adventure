9d1938f53b20880bac77546e4792edebcd463547
seasons986 <annalin12345@gmail.com>
2023-11-07 15:30:04 -0800

    forget it

--- index.js
+++ index.js
@@ -5,20 +5,33 @@ window.addEventListener("load", () => {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
   const container = document.getElementById("container");
+  const overlay = document.getElementById("overlay");
   const rooms = ["moon", "sun", "star", "cloud", "satellite"];
-  const chWidth = 24;
-  const chHeight = 24;
+  const room = "sun"//rooms[getRandomInt(0,rooms.length)];
+  console.log({room});
+  const chWidth = room.length * 9;
+  const chHeight = 19.5;
+  const diameter = 25;
+  const increment = 5;
 
   function initialize() {
-    const room = rooms[getRandomInt(0,rooms.length)];
-    console.log({room});
+    const centerW = Math.floor(window.innerWidth/2);
+    const centerH = Math.floor(window.innerHeight/2);
 
     for (let i = 0; i < window.innerWidth; i+=chWidth) {
       for (let j = 0; j < window.innerHeight; j+=chHeight) {
-        container.innerHTML += `&nbsp;${room}&nbsp;`;
+        if (i >= (centerW-diameter) && i <= (centerW+diameter) 
+          && j > (centerH-diameter) && j <= (centerH+diameter)) {
+        console.log({i, j})
+            // container.innerHTML += `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`;
+            container.innerHTML += `&nbsp;***&nbsp;`;
+        }
+        else {
+          container.innerHTML += `&nbsp;${room}&nbsp;`;
+        }
       }
     }
   }
-
+  // container.innerHTML += `&nbsp;${room}&nbsp;`;
   initialize();
 });
